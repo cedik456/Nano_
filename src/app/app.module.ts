eced32c434c7366ff141016822f9aa170f3d3af0
Cedric Naño <146691376+cedik456@users.noreply.github.com>
2025-01-28 17:14:48 +0800

    Finish using angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post.create.component';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [AppComponent, PostCreateComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  imports: [
+    MatCardModule,
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
