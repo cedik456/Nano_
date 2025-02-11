23e9e60b178066a5881297af257a390993c38e7a
Cedric Naño <146691376+cedik456@users.noreply.github.com>
2025-02-11 17:13:53 +0800

    Activity

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
+interface Post {
+  title: string;
+  content: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular-project';
+  storedPosts: Post[] = [];
+
+  onPostAdded(post: Post): void {
+    this.storedPosts.push(post);
+  }
 }
