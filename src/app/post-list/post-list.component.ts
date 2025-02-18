3f0d70d90b93021fc0a40a9c187051077fabacb7
Cedric Naño <146691376+cedik456@users.noreply.github.com>
2025-02-18 17:12:35 +0800

    Add error handlers and minimum lengths

--- src/app/post-list/post-list.component.ts
+++ src/app/post-list/post-list.component.ts
@@ -1,10 +1,27 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Post } from '../posts/post.model';
+import { PostsService } from '../posts/posts.service';
 
 @Component({
   selector: 'post-list',
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.css'],
 })
-export class PostListComponent {
-  @Input() posts = [{ title: '1st title', content: '1st Content' }];
+export class PostListComponent implements OnInit, OnDestroy {
+  posts: Post[] = [];
+  private postsSub!: Subscription;
+
+  constructor(public postsService: PostsService) {}
+  ngOnInit() {
+    this.posts = this.postsService.getPosts();
+    this.postsSub = this.postsService
+      .getPostUpdatedListener()
+      .subscribe((posts: Post[]) => {
+        this.posts = posts;
+      });
+  }
+  ngOnDestroy() {
+    this.postsSub.unsubscribe;
+  }
 }
