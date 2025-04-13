0640872cbf7db091bd6bb42ec386718f23c775af
Cedric Naño <146691376+cedik456@users.noreply.github.com>
2025-04-13 21:31:33 +0800

    Add user input handler

--- src/app/authentication/login/login.component.ts
+++ src/app/authentication/login/login.component.ts
@@ -1,7 +1,12 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
-export class logincomponent {}
+export class logincomponent {
+  onLogin(form: NgForm) {
+    console.log(form.value);
+  }
+}
