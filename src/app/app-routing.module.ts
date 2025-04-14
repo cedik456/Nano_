4fa6287d1ff00e165bee4a6513054a2f964fdf1e
Cedric Naño <146691376+cedik456@users.noreply.github.com>
2025-04-14 19:26:11 +0800

    Finish adding the signup form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { PostListComponent } from './post-list/post-list.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { logincomponent } from './authentication/login/login.component';
+import { signupcomponent } from './authentication/signup/signup.component';
 
 const routes: Routes = [
   { path: '', component: PostListComponent },
   { path: 'create', component: PostCreateComponent },
   { path: 'edit/:postId', component: PostCreateComponent },
   { path: 'login', component: logincomponent },
+  { path: 'signup', component: signupcomponent },
 ];
 
 @NgModule({
