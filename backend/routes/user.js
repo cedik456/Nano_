f45e665e857746f9cc14458825a680935b32b9ca
Cedric Naño <146691376+cedik456@users.noreply.github.com>
2025-04-15 15:09:50 +0800

    Finish implementing SPA authentication

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -8,9 +8,45 @@ const User = require("../models/user");
 // middlewares
 
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 router.get("/", (req, res) => {
   console.log("Hello User");
+});
+
+router.post("/login", (req, res) => {
+  User.findOne({ email: req.body.email }).then((user1) => {
+    if (!user1) {
+      return res.status(401).json({
+        message: "Auth Failed",
+      });
+    }
+    return bcrypt
+      .compare(req.body.password, user1.password)
+      .then((result) => {
+        if (!result) {
+          return res.status(401).json({
+            message: "Auth Failed",
+          });
+        }
+        const token = jwt.sign(
+          { email: user1.email, userId: user1._id },
+          "A_very_long_string_for_our_secret",
+          { expiresIn: "1h" }
+        );
+
+        res.json({
+          token: token,
+          expiresIn: 3600,
+          userId: user1._id,
+        });
+      })
+      .catch((err) => {
+        return res.status(401).json({
+          message: "Auth Failed",
+        });
+      });
+  });
 });
 
 router.post("/signup", (req, res) => {
