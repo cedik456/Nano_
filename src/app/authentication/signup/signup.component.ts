64c2185ac8ac9ce3e07b9810727f36a2d9e035e4
Cedric Naño <146691376+cedik456@users.noreply.github.com>
2025-04-15 13:19:37 +0800

    Finish connecting forms with backend

--- src/app/authentication/signup/signup.component.ts
+++ src/app/authentication/signup/signup.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { AuthService } from '../auth.service';
 
 @Component({
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css'],
 })
 export class signupcomponent {
+  Loading = false;
+
+  constructor(public authService: AuthService) {}
+
   onSignup(form: NgForm) {
-    console.log(form.value);
+    if (form.invalid) {
+      return;
+    }
+    this.authService.CreateUser(form.value.email, form.value.password);
   }
 }
