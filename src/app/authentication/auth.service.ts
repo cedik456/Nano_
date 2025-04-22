2c52cb1dbf2e0f6587a12fb11347c9d2eb3b93d6
Cedric Naño <146691376+cedik456@users.noreply.github.com>
2025-04-22 13:13:37 +0800

    Improve UI header

--- src/app/authentication/auth.service.ts
+++ src/app/authentication/auth.service.ts
@@ -1,16 +1,24 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { AuthData } from './auth-data.model';
+import { Subject } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
   private token!: string;
+
+  private authStatusListener = new Subject<boolean>();
 
   getToken() {
     return this.token;
   }
 
   constructor(private http: HttpClient) {}
+
+  getAuthStatusListener() {
+    return this.authStatusListener.asObservable();
+  }
+
   CreateUser(email: string, password: string) {
     const authData: AuthData = { email: email, password: password };
     this.http
@@ -29,6 +37,7 @@ export class AuthService {
       .subscribe((response) => {
         const token = response.token;
         this.token = token;
+        this.authStatusListener.next(true);
         console.log('Token received:', this.token);
       });
   }
